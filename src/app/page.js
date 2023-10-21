15d4ad3080f666abb95a7745cb624b49def8a533
Logan Orr <logan_orr@icloud.com>
2023-10-21 15:43:17 -0600

    Mobile and desktop versions complete

--- src/app/page.js
+++ src/app/page.js
@@ -6,78 +6,57 @@ import { SignUpForm } from './SignUpForm'
 
 export default function Home() {
   return (
-    // <div>
-    //   <Head>
-    //     <title>My page title</title>
-    //     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    //   </Head>
-    //   <main className="flex h-screen flex-col pl-64 pr-32 pt-8">
-    //     <div className='flex justify-start items-center h-5/6'>
-    //       <div className='basis-1/2 flex justify-end pr-36'>
-    //         <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
-    //           width="350"
-    //           height="400"
-    //         ></Image>
-    //       </div>
-    //       <div>
-    //         <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
-    //           width="180"
-    //           height="100"></Image>
-    //         <p className='font-semibold text-6xl font-sans pb-2'>Find the right dental<br></br>shift, right away</p>
-    //         <p className='font-medium text-3xl font-sans'>All around America.</p>
-    //         <p className='pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
-    //         <div className='flex flex-col pt-8'>
-    //           <SignUpForm />
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </main>
-    // </div>
+    <div>
+      <div className="desktop">
+        <main className="md:flex md:h-screen md:flex-col md:pl-64 md:pr-32 md:pt-8">
+          <div className='md:flex md:justify-start md:items-center md:h-5/6'>
+            <div className='md:basis-1/2 md:flex md:justify-end md:pr-36'>
+              <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
+                width="350"
+                height="400"
+              ></Image>
+            </div>
+            <div>
+              <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
+                width="180"
+                height="100"></Image>
+              <p className='md:font-semibold md:text-6xl md:font-sans md:pb-2'>Find the right dental<br></br>shift, right away</p>
+              <p className='md:font-medium md:text-3xl md:font-sans'>All around America.</p>
+              <p className='md:pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
+              <div className='md:flex md:flex-col md:pt-8'>
+                <SignUpForm />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
 
-    <main className='pl-8 pr-8'>
-      <div className='flex justify-center'>
-        <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
-          width="150"
-          height="50"></Image>
+      <div className='mobile'>
+        <main className='pl-8 pr-8'>
+          <div className='flex justify-center'>
+            <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
+              width="150"
+              height="50"></Image>
+          </div>
+          <div>
+            <p className='font-semibold text-3xl font-sans pb-2 text-center'>Find the right dental<br></br>shift, right away</p>
+            <p className='font-medium text-2xl font-sans pb-4 text-center'>All around America.</p>
+          </div>
+          <div className='flex justify-center'>
+            <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
+              width="350"
+              height="400"
+            ></Image>
+          </div>
+          <div>
+            <p className='pt-8 text-center'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
+          </div>
+          <div>
+            <SignUpForm />
+          </div>
+        </main>
       </div>
-      <div>
-        <p className='font-semibold text-3xl font-sans pb-2 text-center'>Find the right dental<br></br>shift, right away</p>
-        <p className='font-medium text-2xl font-sans pb-4 text-center'>All around America.</p>
-      </div>
-      <div className='flex justify-center'>
-        <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
-          width="350"
-          height="400"
-        ></Image>
-      </div>
-      <div>
-        <p className='pt-8 text-center'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
-      </div>
-      <div>
-        <SignUpForm />
-      </div>
-    </main>
+    </div>
 
-    // <main className="flex h-screen flex-col pl-64 pr-32 pt-8">
-    //   <div className='flex justify-start items-center h-5/6'>
-    //     <div className='basis-1/2 flex justify-end pr-36'>
-    //       <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
-    //         width="350"
-    //         height="400"
-    //       ></Image>
-    //     </div>
-    //     <div>
-    //       <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
-    //         width="180"
-    //         height="100"></Image>
-    //       <p className='font-semibold text-6xl font-sans pb-2'>Find the right dental<br></br>shift, right away</p>
-    //       <p className='font-medium text-3xl font-sans'>All around America.</p>
-    //       <p className='pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
-    //       <div className='flex flex-col pt-8'>
-    //         <SignUpForm />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </main>
   )
 }
