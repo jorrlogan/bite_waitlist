262e09d868aac3f565b3fc57ab508f7f1bf22948
Logan Orr <logan_orr@icloud.com>
2023-10-21 15:10:52 -0600

    Mobile first version ready

--- src/app/SignUpForm.js
+++ src/app/SignUpForm.js
@@ -35,51 +35,95 @@ export function SignUpForm() {
 
     if (message === 'Join Waitlist') {
         return (
-            <div>
-                <form className="relative isolate mt-8 flex items-center pr-1" onSubmit={handleSubmit}>
-                    <label htmlFor={id} className="sr-only">
-                        Email address
-                    </label>
-                    <input
-                        required
-                        type="email"
-                        autoComplete="email"
-                        name="email"
-                        id={id}
-                        placeholder="Email address"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
-                    />
-                    <input
-                        required
-                        type="text"
-                        autoComplete=""
-                        name="position"
-                        id={id}
-                        placeholder="Work Position"
-                        value={position}
-                        onChange={(e) => setPosition(e.target.value)}
-                        className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
-                    />
-                    <input
-                        required
-                        type="text"
-                        autoComplete="zipcode"
-                        name="zipcode"
-                        id={id}
-                        placeholder="Zip code"
-                        value={zipcode}
-                        onChange={(e) => setZipcode(e.target.value)}
-                        className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
-                    />
-                    <button type="submit" arrow className='p-2 m-2 bg-red-600 text-white font rounded'>
-                        {message}
-                    </button>
-                    <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
-                    <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" />
-                </form>
-            </div>
+            // <div>
+            //     <form className="relative isolate mt-8 flex items-center pr-1" onSubmit={handleSubmit}>
+            //         <label htmlFor={id} className="sr-only">
+            //             Email address
+            //         </label>
+            //         <input
+            //             required
+            //             type="email"
+            //             autoComplete="email"
+            //             name="email"
+            //             id={id}
+            //             placeholder="Email address"
+            //             value={email}
+            //             onChange={(e) => setEmail(e.target.value)}
+            //             className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
+            //         />
+            //         <input
+            //             required
+            //             type="text"
+            //             autoComplete=""
+            //             name="position"
+            //             id={id}
+            //             placeholder="Work Position"
+            //             value={position}
+            //             onChange={(e) => setPosition(e.target.value)}
+            //             className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
+            //         />
+            //         <input
+            //             required
+            //             type="text"
+            //             autoComplete="zipcode"
+            //             name="zipcode"
+            //             id={id}
+            //             placeholder="Zip code"
+            //             value={zipcode}
+            //             onChange={(e) => setZipcode(e.target.value)}
+            //             className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-black placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
+            //         />
+            //         <button type="submit" arrow className='p-2 m-2 bg-red-600 text-white font rounded'>
+            //             {message}
+            //         </button>
+            //         <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
+            //         <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" />
+            //     </form>
+            // </div>
+
+            <div className='pb-20'>
+            <form className="flex flex-col mt-8 flex items-center pr-1" onSubmit={handleSubmit}>
+                <label htmlFor={id} className="sr-only">
+                    Email address
+                </label>
+                <input
+                    required
+                    type="email"
+                    autoComplete="email"
+                    name="email"
+                    id={id}
+                    placeholder="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="border rounded-lg p-2 m-2 w-10/12 md:w-96"
+                />
+                <input
+                    required
+                    type="text"
+                    autoComplete=""
+                    name="position"
+                    id={id}
+                    placeholder="Work Position"
+                    value={position}
+                    onChange={(e) => setPosition(e.target.value)}
+                    className="border rounded-lg p-2 m-2 w-10/12 md:w-96"
+                />
+                <input
+                    required
+                    type="text"
+                    autoComplete="zipcode"
+                    name="zipcode"
+                    id={id}
+                    placeholder="Zip code"
+                    value={zipcode}
+                    onChange={(e) => setZipcode(e.target.value)}
+                    className="border rounded-lg p-2 m-2 w-10/12 md:w-96"
+                />
+                <button type="submit" arrow className='p-2 m-2 bg-red-600 text-white font rounded'>
+                    {message}
+                </button>
+            </form>
+        </div>
         )
     } else {
         return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,83 @@
 'use client'
 
+import Head from 'next/head'
 import Image from 'next/image'
 import { SignUpForm } from './SignUpForm'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col pl-64 pr-64 pt-8">
-      <div className='w-full flex justify-center'>
-        <div className='flex justify-between w-full'>
+    // <div>
+    //   <Head>
+    //     <title>My page title</title>
+    //     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    //   </Head>
+    //   <main className="flex h-screen flex-col pl-64 pr-32 pt-8">
+    //     <div className='flex justify-start items-center h-5/6'>
+    //       <div className='basis-1/2 flex justify-end pr-36'>
+    //         <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
+    //           width="350"
+    //           height="400"
+    //         ></Image>
+    //       </div>
+    //       <div>
+    //         <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
+    //           width="180"
+    //           height="100"></Image>
+    //         <p className='font-semibold text-6xl font-sans pb-2'>Find the right dental<br></br>shift, right away</p>
+    //         <p className='font-medium text-3xl font-sans'>All around America.</p>
+    //         <p className='pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
+    //         <div className='flex flex-col pt-8'>
+    //           <SignUpForm />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </main>
+    // </div>
 
-          {/* <p className='text-red-600 font-bold text-4xl'>BITE</p> */}
-          <div className='basis-1/2 w-full flex justify-start pl-32'>
-            {/* 
-            <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
-              width="70"
-              height="20"></Image> */}
-          </div>
-          {/* <div className='flex'>
-            <p className='text-red-600 pr-16'>Become a Dental Partner</p>
-          </div> */}
-        </div>
+    <main className='pl-8 pr-8'>
+      <div className='flex justify-center'>
+        <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
+          width="150"
+          height="50"></Image>
       </div>
-      <div className='flex justify-start items-center h-5/6'>
-        <div className='basis-1/2 flex justify-end pr-36'>
-          <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
-            width="350"
-            height="400"
-          ></Image>
-        </div>
-        <div>
-          <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
-            width="180"
-            height="100"></Image>
-          <p className='font-semibold text-6xl font-sans pb-2'>Find the right dental<br></br>shift, right away</p>
-          <p className='font-medium text-3xl font-sans'>All around America.</p>
-          <p className='pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
-          <div className='flex flex-col pt-8'>
-            <SignUpForm />
-            {/* <input placeholder='Email' />
-            <input placeholder='Work Position' />
-            <input placeholder='Zip Code' /> */}
-          </div>
-        </div>
+      <div>
+        <p className='font-semibold text-3xl font-sans pb-2 text-center'>Find the right dental<br></br>shift, right away</p>
+        <p className='font-medium text-2xl font-sans pb-4 text-center'>All around America.</p>
+      </div>
+      <div className='flex justify-center'>
+        <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
+          width="350"
+          height="400"
+        ></Image>
+      </div>
+      <div>
+        <p className='pt-8 text-center'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
+      </div>
+      <div>
+        <SignUpForm />
       </div>
     </main>
+
+    // <main className="flex h-screen flex-col pl-64 pr-32 pt-8">
+    //   <div className='flex justify-start items-center h-5/6'>
+    //     <div className='basis-1/2 flex justify-end pr-36'>
+    //       <Image src="https://versity-images.s3.us-west-2.amazonaws.com/bite-app.png"
+    //         width="350"
+    //         height="400"
+    //       ></Image>
+    //     </div>
+    //     <div>
+    //       <Image src='https://versity-images.s3.us-west-2.amazonaws.com/bite-logo-red.png'
+    //         width="180"
+    //         height="100"></Image>
+    //       <p className='font-semibold text-6xl font-sans pb-2'>Find the right dental<br></br>shift, right away</p>
+    //       <p className='font-medium text-3xl font-sans'>All around America.</p>
+    //       <p className='pt-8'>On-demand shifts for dental hygienists, assistants, and receptionists.<br></br>Set your own hours, pay and location with Bite.</p>
+    //       <div className='flex flex-col pt-8'>
+    //         <SignUpForm />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
   )
 }
