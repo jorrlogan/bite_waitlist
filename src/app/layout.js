501aab8bb48613b966e2ffd0280cdd40c7430054
Logan Orr <logan_orr@icloud.com>
2023-11-13 08:21:59 -0800

    Hotjar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,17 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        {/* Add Hotjar tracking code to the head section */}
+        <script>
+          {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3728364,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
